94866706890af91778ff8de34f5af7c5cbe22d2f
frogsword <113356699+frogsword@users.noreply.github.com>
2023-07-07 08:53:05 -0500

    sign, decimal, and percent
    added functionality for sign, decimal, and percent buttons

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
 let allButtons = document.getElementsByTagName('button');
-let operandButtons = document.querySelectorAll('.operand');
-let operatorButtons = document.querySelectorAll('.operator');
-const equalButton = document.querySelector('.equal');
-const decimalButton = document.querySelector('.decimal');
-const percentButton = document.querySelector('.percent');
-const signButton = document.querySelector('.sign');
-const clearButton = document.querySelector('.clear');
 const display = document.querySelector('.display');
 let num1;
 let num2;
 let answer;
+let temp;
 
 for (i = 0; i < allButtons.length; i++) {
 
@@ -25,6 +19,19 @@ for (i = 0; i < allButtons.length; i++) {
                }
           }
 
+//percent
+          if (e.target.getAttribute('class') === 'percent') {
+               temp = parseFloat(display.textContent)/100;
+               if (temp.toString().length < 10) {
+                    display.textContent = temp.toString();
+               }
+          }
+
+//adds decimal
+          if (e.target.getAttribute('class') === 'decimal' && !(display.textContent.includes("."))) {
+               display.textContent += e.target.textContent;
+          }
+
 //clears display
           if (e.target.getAttribute('class') === 'clear') {
                display.textContent = '0';
@@ -32,9 +39,14 @@ for (i = 0; i < allButtons.length; i++) {
                num2 = null;
                answer = null;
           }
+
+//switches sign
+          if (e.target.getAttribute('class') === 'sign') {
+               display.textContent *= -1;
+          }
      })
 
-     //lightens button color when clicked
+//lightens button color when clicked
      allButtons[i].addEventListener('mousedown', function(e) {
           e.target.style.backgroundColor = `hsl(${e.target.getAttribute('color')}, 100%, 90%)`;
      })
@@ -44,5 +56,4 @@ for (i = 0; i < allButtons.length; i++) {
      allButtons[i].addEventListener('mouseout', function(e) {
           e.target.style.backgroundColor = `hsl(${e.target.getAttribute('color')}, 100%, 50%)`;
      })
-     //
 }
